0ae17b2ef54ac97b9d8050836665a38fd5bd3daf
Atharv Salvi <atharv.salvi22@gmail.com>
2025-07-07 19:00:01 +0530

    Changes made - Basic interaction

--- etho.py
+++ etho.py
@@ -1,2 +1,10 @@
 
-print("Hello, this is your bot Etho. How may I help you?")
+print("Hello, this is your bot Etho.")
+print("May I know your name?")
+name = input("Name >> ")
+print(f"Welcome, {name}! How may I help you?")
+while(True) :
+    interaction = input(">> ")
+    if interaction == "exit" :
+        print(f"Goodbye {name}! Remember, always type 'etho' in your powershell and I will be at your service!")
+        break
