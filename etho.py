5626af84befdb751ba93ee5e3d5aaafbab34070b
Atharv Salvi <atharv.salvi22@gmail.com>
2025-07-11 20:38:48 +0530

    Initial commit

--- etho.py
+++ etho.py
@@ -1,13 +1,10 @@
 from gemini import generate
 import os
 
-# print("Hello, this is your bot Etho.")
-# print("May I know your name?")
-# name = input("Name >> ")
-# print(f"Welcome, {name}! How may I help you?")
-# while(True) :
-    # interaction = input(">> ")
-texts = generate().split('\n')
-
-for text in texts :
-    os.system(text)
+session = True
+while(session) :
+    userInput = input(">> ")
+    if(userInput == "exit"): break
+    texts = generate(userInput).split('\n')
+    for text in texts :
+        os.system(text)
--- gemini.py
+++ gemini.py
@@ -6,15 +6,15 @@ from google.genai import types
 
 load_dotenv()
 
-def generate():
+def generate(userInput):
     client = genai.Client(
         api_key = os.environ["GEMINI_API_KEY"]
     )
     response = client.models.generate_content(
         model = "gemini-2.5-flash",
-        contents=" add and commit to git with message add and commit from the code itself",
+        contents=f"userInput",
         config=types.GenerateContentConfig(
-            system_instruction = "Give the response in the form of powershell commands only. Do not add any additional content or markdown."
+            system_instruction = "Give the response in the form of git commands only. Do not add any additional content or markdown."
         ),
     )
     return response.text
